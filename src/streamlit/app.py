205f63d2d2580f3922e340b5e50a8ab8774ce4b0
unknown <cansuarpaciogluq@gmail.com>
2025-01-29 22:19:29 +0100

    path_correction

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -747,7 +747,7 @@ elif section == "Genre Detection":
         st.markdown("""Parameters (determined with GridSearch): n_neighbors = 30, metric = "manhattan" """)
         st.subheader("Classification Report")
         st.image("src/streamlit/data/ClassificationReportKNN.PNG")
-        st.image("src/streamlit/data/Confusion_Matrix_KNN.PNG")
+        st.image("src/streamlit/data/ConfusionMatrixKNN.PNG")
         
         
     elif option == 'Decision Tree':
